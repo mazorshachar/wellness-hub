50e8e76992fa43a1da164e78ded16f2c9420a9ab
Shahar Mazur <Mazorshachar@gmail.com>
2026-08-15 22:01:48 -0500

    key fix2

--- vitals/app/src/main/java/com/vitals/app/ui/DashboardScreen.kt
+++ vitals/app/src/main/java/com/vitals/app/ui/DashboardScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.item
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
